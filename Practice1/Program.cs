a90e49d855eb1bf697ab034f572a5e521bc4ef3c
nureevmarsel <mnurev@mail.ru>
2018-06-12 17:07:54 +0500

    firstcommit

---
+++ Practice1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество элементов N: ");
+            bool ok;
+            int n;
+            do
+            {
+                ok = Int32.TryParse(Console.ReadLine(), out n);
+                if (!ok)
+                    Console.WriteLine("Ошибка ввода. Введите натуральное число.");
+                if (n <= 0)
+                {
+                    ok = false;
+                    Console.WriteLine("Ошибка ввода. Введите натуральное число");
+                }
+            } while (!ok);
+            int[] arr = new int[n];
+            Console.WriteLine("Вводите элементы массива через Enter:");
+            ok = false;
+            int el;
+            for (int i = 0; i < n; i++)
+            {
+                do
+                {
+                    ok = Int32.TryParse(Console.ReadLine(), out el);
+                    if (!ok)
+                        Console.WriteLine("Ошибка ввода. Введите целое число.");
+                } while (!ok);
+                arr[i] = el;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        arr[i] = arr[j];
+                        arr.Concat()
+                    }
+                }
+            }
+        }
+    }
+}
